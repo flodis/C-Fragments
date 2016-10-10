cbafa46777acd5a574fe2ed21a59f0d6e6b975dc
flodis <flodmotorgrodan@gmail.com>
2016-10-10 12:36:13 +0200

    All done. And perfect?

---
+++ Enum/EnumListLambda.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+
+class IENumDemo
+{
+
+    /// <summary>
+    /// Create a cosinus table enumerator with 0..360 deg values
+    /// </summary>
+    private IEnumerator costable = new Func<List<float>>(() =>
+        {
+            List<float> nn = new List<float>();
+            for (int v = 0; v < 360; v++)
+            {
+                nn.Add((float)Math.Cos(v * Math.PI / 180));
+            }
+
+            return nn;
+        }
+
+        )().GetEnumerator();
+
+
+    /// <summary>
+    /// Demonstrates eternal fetch of next value from an IEnumerator
+    /// At end of list the enumerator is reset to start of list
+    /// </summary>
+    /// <returns></returns>
+    private float GetaNum()
+    {
+        //Advance to next item
+        if (!costable.MoveNext())
+        {
+            //End of list - reset and advance to first
+            costable.Reset();
+            costable.MoveNext();
+        }
+
+        //Return Enum current value
+        return costable.Current;
+    
+    
+    }
+
+}
