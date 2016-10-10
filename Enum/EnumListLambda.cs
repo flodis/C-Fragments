edbe71384b59470606ead181a7c15d981b7b3b1c
flodis <flodmotorgrodan@gmail.com>
2016-10-10 13:22:08 +0200

    Improved IEnumerable and IEnumerator sample code

--- Enum/EnumListLambda.cs
+++ Enum/EnumListLambda.cs
@@ -3,26 +3,28 @@ using System.Collections;
 class IENumDemo
 {
 
+
     /// <summary>
     /// Create a cosinus table enumerator with 0..360 deg values
     /// </summary>
-    private IEnumerator costable = new Func<List<float>>(() =>
-        {
-            List<float> nn = new List<float>();
-            for (int v = 0; v < 360; v++)
+    private IEnumerable<float> cosdata = new Func<IEnumerable<float>>(() =>
             {
-                nn.Add((float)Math.Cos(v * Math.PI / 180));
+                for (int v = 0; v < 360; v++)
+                {
+                    yield return (float)Math.Cos(v * Math.PI / 180);
+                }
             }
+        )();
 
-            return nn;
-        }
-
-        )().GetEnumerator();
+    /// <summary>
+    /// Create a cosinus table IEnumerator from the cosdata table
+    /// </summary>
+    private IEnumerator cosenum = cosdata.GetEnumerator();
 
 
     /// <summary>
-    /// Demonstrates eternal fetch of next value from an IEnumerator
-    /// At end of list the enumerator is reset to start of list
+    /// Demonstrates eternal fetch of one value at a time from an IEnumerator
+    /// If end of list the enumerator is reset to the enumeration start
     /// </summary>
     /// <returns></returns>
     private float GetaNum()
@@ -40,5 +42,22 @@ class IENumDemo
     
     
     }
+
+
+    /// <summary>
+    /// Basic demo how to construct an IEnumerable from a loop
+    /// Can be useful if only a few values at the begining of a collection is normally used
+    /// or to deliver continuos chunks of data from a source in a streaming manner.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<float> GetaList()
+    {
+        for (int v = 0; v < 360; v++)
+        {
+            yield return (float)Math.Cos(v * Math.PI / 180);
+        }
+
+    }
+
 
 }
